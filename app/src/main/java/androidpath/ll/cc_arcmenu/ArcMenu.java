ce8d52a23cdcbe712b98b8f593ebb633b7d98499
leonard-lin <linle.job@gmail.com>
2015-05-31 13:42:15 -0700

    create ArcMenu class

---
+++ app/src/main/java/androidpath/ll/cc_arcmenu/ArcMenu.java
@@ -0,0 +1,20 @@
+package androidpath.ll.cc_arcmenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Le on 2015/5/31.
+ */
+public class ArcMenu extends ViewGroup {
+
+    public ArcMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
